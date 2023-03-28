fa13dd2c88cad411fc9ebbc9bf1e7441686e3f1b
Owen Krueger <github@owenkrueger.dev>
2023-03-28 09:50:19 -0500

    Add AppDaemon and porch light automation

---
+++ appdaemon/apps/front_porch.py
@@ -0,0 +1,46 @@
+import hassapi as hass
+import datetime
+
+class FrontPorch(hass.Hass):
+    
+    """
+    Sets up the automation.
+    """
+    def initialize(self):
+        time = self.parse_time(self.get_state(self.args["time"]))
+
+        self.run_at_sunset(self.turn_on_front_porch, offset = datetime.timedelta(minutes =- 45).total_seconds())
+        self.handle = self.run_daily(self.turn_off_front_porch, time)
+        self.listen_state(self.time_change, self.args["time"], duration = 60)
+
+    """
+    On time change, cancel the timer and re-set it up so it executes
+    at the new time.
+    """
+    def time_change(self, entity, attribute, old, new, kwargs):
+        self.log('Setting new execution time: {}'.format(new))
+        self.cancel_timer(self.handle)
+        self.handle = self.run_daily(self.turn_off_front_porch, new)
+
+    """
+    Turns on the front porch lights if they are off.
+    """
+    def turn_on_front_porch(self, kwargs):
+        if self.get_state(self.args["switch"]) == "off":
+            self.log('Turning porch lights on.')
+            self.turn_on(self.args["switch"])
+
+    """
+    Turns off the front porch lights if they are on.
+    Will also reset the execution time for future runs if time was
+    overridden for this run.
+    """
+    def turn_off_front_porch(self, kwargs):
+        if self.get_state(self.args["switch"]) == "on":
+            self.log('Turning porch lights off.')
+            self.turn_off(self.args["switch"])
+
+        if self.get_state(self.args["boolean"]):
+            self.log('Time overriden. Resetting execution time to default.')
+            self.set_state(self.args["boolean"], state="off")
+            self.set_state(self.args["time"], state=datetime.time(22, 0, 0))
