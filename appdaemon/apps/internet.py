8b41eeef272ac648f10680a2fcf1b0fe41ed717a
Owen Krueger <github@owenkrueger.dev>
2023-07-09 10:11:05 -0500

    Restart router if internet still off

--- appdaemon/apps/internet.py
+++ appdaemon/apps/internet.py
@@ -8,7 +8,7 @@ class Internet(hass.Hass):
     """
     Sets up the automation.
     """
-    async def initialize(self):
+    def initialize(self):
         self.utils = self.get_app("utils")
         self.internet_up = self.args["internet_up"]
         self.restart_modem_script = self.args["restart_modem_script"]
@@ -25,28 +25,36 @@ class Internet(hass.Hass):
     Restarts modem smart plug. Then, sets a callback to check if the internet
     is up in 5 minutes or restarts router.
     """
-    async def restart_modem(self, entity, attribute, old, new, kwargs):
-        await self.restart_entity(self.internet_modem_smart_plug)
+    def restart_modem(self, entity, attribute, old, new, kwargs):
+        self.restart_entity(self.internet_modem_smart_plug)
 
         self.run_in(self.restart_router, 33) # Restart router if internet still down in 5.5 minutes.
 
     """
     Restarts router smart plug.
     """
-    async def restart_router(self, cb_args):
+    def restart_router(self, cb_args):
         if (not self.utils.is_entity_on(self.internet_up)):
-            await self.restart_entity(self.internet_router_smart_plug)
+            self.restart_entity(self.internet_router_smart_plug)
 
     """
-    Restarts the input entity and notifies Owen.
+    Restarts input entity by turning it off and then scheduling a callback to
+    run in 15 seconds to turn the entity back on.
     """
-    async def restart_entity(self, entity):
+    def restart_entity(self, entity):
         if (self.utils.recently_triggered(entity, 30)):
             self.log("{} already manually restarted. Not restarting.".format(entity))
             return
 
         self.log("Restarting {}".format(entity))
         self.turn_off(entity)
-        await self.sleep(15)
+        self.run_in(self.turn_on_entity, 15, entity = entity)
+
+    """
+    Turns input entity back on.
+    """
+    def turn_on_entity(self, kwargs):
+        entity = kwargs["entity"]
+
         self.turn_on(entity)
         self.notify("Restarted {} due to internet outage.".format(entity))
