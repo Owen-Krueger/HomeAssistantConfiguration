a94f17e02d70f6dd58b26b8906b97b37f76ba919
Owen Krueger <github@owenkrueger.dev>
2023-07-14 09:31:40 -0500

    Clean up weird edge cases in automations

--- appdaemon/apps/cameras.py
+++ appdaemon/apps/cameras.py
@@ -11,28 +11,46 @@ class Cameras(hass.Hass):
     def initialize(self):
         self.utils = self.get_app("utils")
         self.allison = self.args["allison"]
-        self.owen = self.args["owen"]
         self.cameras = self.args["cameras"]
+        self.owen = self.args["owen"]
+        self.proximity_allison = self.args["proximity_allison"]
+        self.proximity_owen = self.args["proximity_owen"]
 
-        self.listen_state(self.turn_off_on_cameras, self.allison, new = "home")
-        self.listen_state(self.turn_off_on_cameras, self.owen, new = "home")
-        self.listen_state(self.turn_off_on_cameras, self.allison, new = "not_home", duration = 600) # When away for 10 minutes.
-        self.listen_state(self.turn_off_on_cameras, self.owen, new = "not_home", duration = 600) # When away for 10 minutes.
+        self.listen_state(self.turn_off_cameras, self.allison, new = "home")
+        self.listen_state(self.turn_off_cameras, self.owen, new = "home")
+        self.listen_state(self.turn_on_cameras, self.proximity_allison, new = 30)
+        self.listen_state(self.turn_on_cameras, self.proximity_owen, new = 30)
+
+    """
+    Turns on the cameras if nobody is home and the triggered is moving away.
+    """
+    def turn_on_cameras(self, entity, attribute, old, new, kwargs):
+        direction = self.get_state(entity, attribute="dir_of_travel")
+        if direction != "towards" or self.someone_home():
+            return
+        
+        self.turn_off_on_cameras(True)
+
+    """
+    Turns off the cameras if someone is home.
+    """
+    def turn_off_cameras(self, entity, attribute, old, new, kwargs):
+        if not self.someone_home():
+            return
+
+        self.turn_off_on_cameras(False)
 
     """
     Turns off cameras if anyone home and cameras are on.
     Turns on cameras if everyone away from home and cameras are off.
     """
-    def turn_off_on_cameras(self, entity, attribute, old, new, kwargs):
-        owen_home = self.utils.is_entity_home(self.owen)
-        allison_home = self.utils.is_entity_home(self.allison)
-        someone_home = owen_home or allison_home
-        camera_state_log_message = "off" if someone_home else "on"
+    def turn_off_on_cameras(self, turn_on: bool):
+        camera_state_log_message = "on" if turn_on else "off"
 
         cameras_updated = False
         self.log("Turning {} cameras due to occcupancy.".format(camera_state_log_message))
         for camera in self.cameras:
-            cameras_updated = self.turn_off_on_camera(camera, not someone_home) or cameras_updated
+            cameras_updated = self.turn_off_on_camera(camera, turn_on) or cameras_updated
 
         if cameras_updated:
             self.notify("Cameras turned {}.".format(camera_state_log_message), name="owen")
@@ -49,3 +67,9 @@ class Cameras(hass.Hass):
             return True
         
         return False
+
+    """
+    If someone is actively home.
+    """
+    def someone_home(self) -> bool:
+        return self.utils.is_entity_home(self.allison) or self.utils.is_entity_home(self.owen)
--- appdaemon/apps/toggleable_lighting.py
+++ appdaemon/apps/toggleable_lighting.py
@@ -12,20 +12,27 @@ class ToggleableLighting(hass.Hass):
         self.utils = self.get_app("utils")
 
         for event in self.args["dictionary"]:
-            self.listen_event(self.toggle_light, "zha_event", device_id = event["event_device_id"], command = event["command"], light = event["light"])
+            self.listen_event(self.toggle_light, "zha_event", device_id = event["event_device_id"], command = event["command"], lights = event["lights"])
 
     """
     Turns light on if currently off and turns light off if currently on.
     """
     def toggle_light(self, event_name, data, kwargs):
-        light = kwargs["light"]
-        
+        lights = kwargs["lights"]
+
+        first_light = lights[0]
         # Prevents duplicate events from toggling the light more than once.
-        if self.utils.recently_triggered(light):
-            self.log("{} recently triggered. Not toggling.".format(light))
+        if self.utils.recently_triggered(first_light):
+            self.log("{} recently triggered. Not toggling.".format(lights))
             return
 
-        current_state = self.utils.is_entity_on(light)
-        self.log("Toggle triggered for {}. Turning light {}.".format(light, "off" if current_state else "on"))
+        # This is a work around where sometimes lights get out of sync.
+        current_state = self.utils.is_entity_on(first_light)
+        self.log("Toggle triggered for {}. Turning light {}.".format(lights, "off" if current_state else "on"))
 
-        self.toggle(light)
+        # Go through the lights and turn them all on/off.
+        for light in lights:
+            if current_state:
+                self.turn_off(light)
+            else:
+                self.turn_on(light)
