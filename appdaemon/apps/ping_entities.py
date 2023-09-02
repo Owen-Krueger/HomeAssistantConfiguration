e54bb6c251d4c86268356db961eeb8d719655752
Owen Krueger <github@owenkrueger.dev>
2023-09-02 10:17:56 -0500

    Why are kwargs

--- appdaemon/apps/ping_entities.py
+++ appdaemon/apps/ping_entities.py
@@ -17,18 +17,20 @@ class PingEntities(hass.Hass):
     """
     Wraps `ping_entity_inner`. This is called by `listen_state`.
     """
-    def ping_entity(self, entity, attribute, old, new, kwargs):
-        kwargs["entity"] = entity # Add entity to kwargs for inner method to use.
-        self.ping_entity_inner(kwargs)
-
+    def ping_entity(self, entity, attribute, old, new, **kwargs):
+        kwargs["entity"] = entity
+        self.ping_entity_inner(**kwargs)
+    
     """
     Pings the entity and waits 5 seconds to see if it comes online.
     """
-    def ping_entity_inner(self, kwargs):
-        self.log("Pinging {} because it's unavailable".format(kwargs["entity"]))
+    def ping_entity_inner(self, **kwargs):
+        entity = kwargs["entity"]
+        ping = kwargs["ping"]
+        self.log("Pinging {} because it's unavailable".format(entity))
 
-        self.call_service("button/press", entity_id = kwargs["ping"])
-        self.run_in(self.ensure_entity_on, 5, kwargs) # Wait 5 seconds.
+        self.call_service("button/press", entity_id = ping)
+        self.run_in(self.ensure_entity_on, 2, **kwargs)
 
     """
     Checks if the entity has stopped being unavailable. If `sync_entity` provided,
@@ -36,22 +38,21 @@ class PingEntities(hass.Hass):
     if one of two lamps goes unavaliable. This should get it online and set to
     the expected state.
     """
-    def ensure_entity_on(self, kwargs):
+    def ensure_entity_on(self, **kwargs):
         entity = kwargs["entity"]
-        count = kwargs["count"]
+        sync_entity = kwargs["sync_entity"]
+        count = int(kwargs["count"])
 
         if self.get_state(entity) == "unavailable": # Ping didn't fix state.
             self.log("{} pinged but still unavailable. (Count: {})".format(entity, count))
 
-            if int(count) >= 3:
+            if count >= 3:
                 self.notify("{} pinged multiple times but still unavailable.".format(entity), name = "owen")
                 return
             
-            new_count = str(int(count) +1)
-            kwargs["count"] = new_count # Sets updated count to pass to `ping_entity_inner`.
-            self.run_in(self.ping_entity_inner, 300, kwargs) # Wait 5 minutes.
+            kwargs["count"] = count + 1
+            self.run_in(self.ping_entity_inner, 300, **kwargs) # Wait 5 minutes.
 
         # If available, check if there's a second entity to sync state with.
-        sync_entity = kwargs["sync_entity"]
         if sync_entity != None:
             self.utils.sync_entities(sync_entity, entity)
